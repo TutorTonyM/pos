3ee77394995a098f4f298454bfeeb0b8136f8317
TutorTonyM <tonym@tutortonym.com>
2021-02-16 12:27:09 -0800

    Botones de control creados.

--- resources/views/tablero/es/parciales/barra-de-control.blade.php
+++ resources/views/tablero/es/parciales/barra-de-control.blade.php
@@ -1,5 +1,16 @@
 <nav id="barra-de-navegacion">
     <a href="#">
-        {{ __('Cash Register') }}
+        <i class="fas fa-cash-register"></i>
+        {{ __('Cash Register') }}      
+    </a>
+
+    <a href="#">
+        <i class="fas fa-boxes"></i>
+        {{ __('Products') }}      
+    </a>
+
+    <a href="#">
+        <i class="fas fa-box-open"></i>
+        {{ __('Providers') }}      
     </a>
 </nav>
