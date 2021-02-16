52dd287c6ab1792a3a6038ecfbb702013b9cfac9
TutorTonyM <tonym@tutortonym.com>
2021-02-16 11:34:43 -0800

    Documentos scss globales agredados y cambio de estructura.

--- public/js/app.js
+++ public/js/app.js
@@ -100,12 +100,9 @@ __webpack_require__.r(__webpack_exports__);
 /*!*********************************!*\
   !*** ./resources/scss/app.scss ***!
   \*********************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Can't find stylesheet to import.\n   ╷\n14 │ @import 'temas/tablero-simple/tablero-simple-central';\r\n   │         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n   ╵\n  C:\\wamp\\www\\pos\\resources\\scss\\app.scss 14:9  root stylesheet\n    at processResult (C:\\wamp\\www\\pos\\node_modules\\webpack\\lib\\NormalModule.js:598:19)\n    at C:\\wamp\\www\\pos\\node_modules\\webpack\\lib\\NormalModule.js:692:5\n    at C:\\wamp\\www\\pos\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\wamp\\www\\pos\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\wamp\\www\\pos\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\wamp\\www\\pos\\node_modules\\sass-loader\\dist\\index.js:73:7\n    at Function.call$2 (C:\\wamp\\www\\pos\\node_modules\\sass\\sass.dart.js:91717:16)\n    at _render_closure1.call$2 (C:\\wamp\\www\\pos\\node_modules\\sass\\sass.dart.js:80362:12)\n    at _RootZone.runBinary$3$3 (C:\\wamp\\www\\pos\\node_modules\\sass\\sass.dart.js:27258:18)\n    at _FutureListener.handleError$1 (C:\\wamp\\www\\pos\\node_modules\\sass\\sass.dart.js:25786:19)");
 
 /***/ })
 
